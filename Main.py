6ec581e3fc3f93d339110f41aa205fb732bd570c
CodyM4V3R1CK <bobkomatej1@gmail.com>
2025-03-13 11:17:22 +0100

    reading a website

---
+++ Main.py
@@ -0,0 +1,14 @@
+import requests
+
+URL = "https://www.prospektmaschine.de/hypermarkte/"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+response = requests.get(URL, headers=HEADERS)
+
+# Save the raw HTML to file
+with open("debug.html", "w", encoding="utf-8") as f:
+    f.write(response.text)
+
+print("HTML saved to debug.html")
